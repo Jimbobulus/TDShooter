a56dfb5650c793ada25fbc370055ae1e7b7297a6
Jimbobulus <Jimbobulus@users.noreply.github.com>
2017-09-15 10:14:26 +0100

    Added text to the avatar toggle button

--- Assets/Scripts/AvatarToggle.cs
+++ Assets/Scripts/AvatarToggle.cs
@@ -7,10 +7,12 @@ public class AvatarToggle : MonoBehaviour {
 	public Toggle toggle;
 
 	private Image background;
+    Text textField;
 
 	void Start () 
 	{
 		background = toggle.transform.Find( "Background" ).GetComponent<Image>() as Image;
+        textField = toggle.transform.Find("Text").GetComponent<Text>() as Text;
 		OnTogglePressed();
 	}
 
@@ -19,11 +21,15 @@ public class AvatarToggle : MonoBehaviour {
 		if( toggle.isOn )
 		{
 			background.color = Color.red;
+            textField.text = "Shotgun";
+            textField.color = Color.black;
 		}
 		else
 		{
 			background.color = Color.blue;
-		}
+            textField.text = "Rifle";
+            textField.color = Color.white;
+        }
 	}
 
 	void Update () 
